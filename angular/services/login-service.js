151100ad3fbf5cc5d4b174985843764ebd78e164
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-21 14:14:25 -0700

    Added endpoint path to login service

--- angular/services/login-service.js
+++ angular/services/login-service.js
@@ -1,5 +1,5 @@
 app.service("LoginService", function($http) {
-	this.LOGIN_ENDPOINT = ""; // TODO
+	this.LOGIN_ENDPOINT = "../../php/api/authenticate/"; // TODO
 
 	this.login = function(loginData) {
 		return ($http.post(this.LOGIN_ENDPOINT, loginData)
