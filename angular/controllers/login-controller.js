089d249a3f91d293e728cc38061196a4d8c819ea
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-21 14:31:53 -0700

    Added login method to login controller

--- angular/controllers/login-controller.js
+++ angular/controllers/login-controller.js
@@ -1,3 +1,16 @@
-app.controller("LoginController", ["$scope", function($scope) {
-	// TODO
+app.controller("LoginController", ["$scope", "LoginService", function($scope, LoginService) {
+	$scope.alerts = [];
+
+	$scope.login = function(loginData, validated) {
+		if(validated === true) {
+			LoginService.login(loginData)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	}
 }]);
