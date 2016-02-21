accb4be3ef83d70475e6300d4d7a72f1efb1dc0d
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-21 16:01:16 -0700

    Added meow function

--- angular/controllers/meow-controller.js
+++ angular/controllers/meow-controller.js
@@ -10,7 +10,20 @@ app.controller("MeowController", ["$scope", "MeowService", function($scope, Meow
 			});
 	};
 
-	$scope.meow = function(channel, message) {
-		// TODO
+	$scope.meow = function(message, validated) {
+		if(validated === true) {
+			MeowService.meow(message)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
 	};
+
+	if($scope.channels.length === 0) {
+		$scope.channels = $scope.getChannels();
+	}
 }]);
